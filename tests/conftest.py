148ca505404ea81696f3271623870dd4f0423679
Adshlsn <2430728556@qq.com>
2026-04-26 12:00:44 +0000

    add backend FastAPI tests

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from copy import deepcopy
+
+from src.app import activities
+
+INITIAL_ACTIVITIES = deepcopy(activities)
+
+
+@pytest.fixture(autouse=True)
+def reset_activities():
+    activities.clear()
+    activities.update(deepcopy(INITIAL_ACTIVITIES))
+    yield
